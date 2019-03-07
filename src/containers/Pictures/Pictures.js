70ecee0ccd94ab123991d1921e8110c768fa14e2
mkubibaev <mkubibaev@gmail.com>
2019-03-07 17:32:52 +0600

    added main container and component

---
+++ src/containers/Pictures/Pictures.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+
+class Pictures extends Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
+
+export default Pictures;
